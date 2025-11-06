e6744d3a4593120702b370a205d749e6555e5575
Osmara <dsi85944@educastur.es>
2025-11-06 12:43:50 +0100

    Proyecto iniciado y documentacion del Hito 1 añadida

---
+++ PROYECTO_MATEOSRAMOS_OSCAR/src/main/java/com/oscar/PROYECTO_MATEOSRAMOS_OSCAR/App.java
@@ -0,0 +1,13 @@
+package com.oscar.PROYECTO_MATEOSRAMOS_OSCAR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
